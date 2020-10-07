32d672abe00aa9b8d6efbe06290c66a7334f99ec
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2020-10-07 15:50:04 -0400

    FC: finished exercice 1

--- exercice.py
+++ exercice.py
@@ -2,13 +2,21 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math
 
 
 # TODO: Définissez vos fonction ici
+def ellipsoïde(a,b,c,masse_volume):
+    volume = (4/3)*math.pi*a*b*c
+    masse = masse_volume*volume
+    answer = (f'volume = {volume}, masse = {masse}') 
+    return answer
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    for i in range(5):
+        a, b, c, masse = i, i+1, 2*i, 5*i
+        print(ellipsoïde(a,b,c,masse))
 
-    pass
+    
